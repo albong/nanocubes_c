11cd770da7d51cb1631a9762e69754adfc0a8ddd
albee <bongio.alex@gmail.com>
2015-08-03 22:31:04 -0400

    Timeseries now has moving start and end indices

--- timeseries.c
+++ timeseries.c
@@ -6,18 +6,26 @@ Timeseries *newTimeseries(){
     Timeseries *result = malloc(sizeof(Timeseries));
     result->bins = NULL;
     result->numBins = 0;
+    result->startBin = -1;
     return result;
 }
 
-void addToTimeseries(Timeseries *self, int time, unsigned long long count){
-    if (time+1 > self->numBins){
+void addToTimeseries(Timeseries *self, size_t time, unsigned long long count){
+    if (time < self->startBin || time >= (self->startBin + self->numBins)){
+        self->bins = realloc(self->bins, sizeof(unsigned long long) * self->numBins);
+        self->startBin = (time < self->startBin) ? time : self->startBin;
+        self->numBins = (time >= (self->startBin + self->numBins)) ? (time - self->startBin + 1) : self->numBins;
+    }
+
+/*    if (time+1 > self->numBins){
         self->bins = realloc(self->bins, sizeof(unsigned long long) * (time+1));
         self->numBins = time + 1;
-    }
+    }*/
+
     self->bins[time] = count;
 }
 
-unsigned long long getCountAtTime(Timeseries *self, int time){
+unsigned long long getCountAtTime(Timeseries *self, size_t time){
     unsigned long long result = 0;
     if (time < self->numBins){
         result = self->bins[time];
@@ -27,8 +35,8 @@ unsigned long long getCountAtTime(Timeseries *self, int time){
 
 void printTimeseries(Timeseries *self){
     int i;
-    printf("numBins: %d; ", self->numBins);
-    for (i = 0; i < self->numBins; i++){
+    printf("startBin: %d; numBins: %d; ", self->startBin, self->numBins);
+    for (i = self->startBin; i < (self->startBin + self->numBins); i++){
         printf("%d:%lu ", i, self->bins[i]);
     }
     printf("\n");
@@ -37,10 +45,11 @@ void printTimeseries(Timeseries *self){
 Timeseries *deepCopyTimeseries(Timeseries *self){
     Timeseries *copy = malloc(sizeof(Timeseries));
 
+    copy->startBin = self->startBin;
     copy->numBins = self->numBins;
     copy->bins = malloc(sizeof(unsigned long long) * copy->numBins);
     size_t i;
-    for (i = 0; i < copy->numBins; i++){
+    for (i = copy->startBin; i < (copy->startBin + copy->numBins); i++){
         copy->bins[i] = self->bins[i];
     }
 
--- timeseries.h
+++ timeseries.h
@@ -5,12 +5,14 @@
 
 typedef struct Timeseries {
     unsigned long long *bins; //array
+    size_t startBin;
     size_t numBins;
+    //[start, end)
 } Timeseries;
 
 Timeseries *newTimeseries();
-void addToTimeseries(Timeseries *self, int time, unsigned long long count);
-unsigned long long getCountAtTime(Timeseries *self, int time);
+void addToTimeseries(Timeseries *self, size_t time, unsigned long long count);
+unsigned long long getCountAtTime(Timeseries *self, size_t time);
 void printTimeseries(Timeseries *self);
 Timeseries *deepCopyTimeseries(Timeseries *self);
 
