0457995ed51ffb227f6081e41093619fad963c36
albee <bongio.alex@gmail.com>
2015-08-09 16:17:00 -0400

    Removed old and unused struct.

--- nckey.h
+++ nckey.h
@@ -3,16 +3,6 @@
 
 //merge all of this into ncnode.h
 
-typedef struct GeoKey {
-    char *xy;
-    unsigned char z;
-} GeoKey;
-
-typedef struct CatKey {
-    char *category;
-    unsigned char numBytes;
-} CatKey;
-
 unsigned char *newGeoKey(unsigned long long x, unsigned long long y, unsigned char z);
 unsigned char *newCatKey(unsigned long long category);
 
